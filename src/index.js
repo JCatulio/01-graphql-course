f4537ce9376a230d392e1dc168f61f797dbfd042
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-22 17:20:56 -0300

    11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello world!',
+      hi: () => 'Hi world!',
+    }
+  }
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
