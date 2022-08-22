8f0736ff5f3c56c010647a65020e00edfc2ca1e1
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-22 17:52:42 -0300

    12

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
-      hi: String
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
     }
   `,
   resolvers: {
     Query: {
-      hello: () => 'Hello world!',
-      hi: () => 'Hi world!',
-    }
-  }
+      id: () => '123',
+      name: () => 'John Doe',
+      age: () => 42,
+      average: () => 3.14,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => {
