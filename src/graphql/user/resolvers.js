1d8b3bf4a8b2606c69fd7e43ae1f2844a003e794
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-23 14:05:35 -0300

    22

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -1,6 +1,7 @@
-const user = async (_, __, { fetch }) => {
-  const user = await fetch('http://localhost:3000/users/602');
-  return user.json();
+const user = async (_, { id }, { fetch }) => {
+  const response = await fetch('http://localhost:3000/users/' + id);
+  const user = await response.json();
+  return user;
 };
 
 const users = async (_, __, { fetch }) => {
