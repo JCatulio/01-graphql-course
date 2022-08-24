0ff862ae11b233ade1c2aad8ec9d9fd5ed84853d
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-24 18:41:47 -0300

    Mutation updatePost e input de dados

--- src/graphql/post/utils/post-repository.js
+++ src/graphql/post/utils/post-repository.js
@@ -2,13 +2,41 @@ import { ValidationError } from 'apollo-server-errors';
 
 export const createPostFn = async (postData, dataSource) => {
   const postInfo = await createPostInfo(postData, dataSource);
-  const { title, body, userId } = await createPostInfo(postData, dataSource);
+  const { title, body, userId } = postInfo;
 
   if (!title || !body || !userId) {
     throw new ValidationError('You have to send title, body and userId');
   }
 
   return await dataSource.post('', { ...postInfo });
+};
+
+export const updatePostFn = async (postId, postData, dataSource) => {
+  if (!postId) {
+    throw new ValidationError('Missing postId');
+  }
+
+  const { title, body, userId } = postData;
+
+  if (typeof title !== 'undefined') {
+    if (!title) {
+      throw new ValidationError('title missing');
+    }
+  }
+
+  if (typeof body !== 'undefined') {
+    if (!body) {
+      throw new ValidationError('body missing');
+    }
+  }
+  if (typeof userId !== 'undefined') {
+    if (!userId) {
+      throw new ValidationError('userId missing');
+    }
+    await userExists(userId, dataSource);
+  }
+
+  return dataSource.patch(postId, { ...postData });
 };
 
 const userExists = async (userId, dataSource) => {
