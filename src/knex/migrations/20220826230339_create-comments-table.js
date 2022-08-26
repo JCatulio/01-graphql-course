3105e2fbf82dd257fb17c5b60544df4e030f4ea1
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-26 20:15:39 -0300

    Migration no KQB

---
+++ src/knex/migrations/20220826230339_create-comments-table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  return knex.schema.createTable('comments', (table) => {
+    table.increments('id');
+    table.text('comment').notNullable();
+    table.string('post_id', 255).notNullable();
+    table.string('user_id', 255).notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  return knex.schema.dropTable('comments');
+};
